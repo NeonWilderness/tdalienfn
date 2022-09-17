582cf8fcaffda08ab69d8a8df91e26f5f295170b
NeonWilderness <neonwilderness@ymail.com>
2022-09-17 14:16:23 +0200

    Initial commit

---
+++ netlify/README.md
@@ -0,0 +1,4 @@
+# Alien inside Twoday
+"Alien Inside Twoday" is a special layout (zip file), that allows [Twoday](https://twoday.net) blogger to embed their new (external) blog within their old/former Twoday blog site in a synced way.
+
+This "serverless" [Netlify function](https://www.netlify.com/products/functions/) provides a JSON parsed/transformed RSS from an external blog feed (e.g. Wordpress, Tumblr, Blogger...) to enable story/article sychronization with Twoday.
---
+++ netlify/functions/getrss.js
@@ -0,0 +1,46 @@
+/**
+ * Netlify function: Read RSS from url and return it as JSON
+ * > As of version 1.11.0 (and the end of webtask.io) this serverless function was migrated to Netlify
+ * > Called: http://localhost:8888/.netlify/functions/getrss?alias={someAlias}&url={someRSSUrl}
+ */
+
+const Parser = require('rss-parser');
+const parser = new Parser({
+  customFields: {
+    item: [
+      ['slash:comments', 'commentCount'],
+      ['thr:total', 'commentCount'],
+      ['atom:summary', 'contentSnippet'],
+      ['post-id', 'postid']
+    ]
+  }
+});
+
+exports.handler = async (event, context) => {
+  try {
+    const ref = event.headers.referrer || 'none';
+    console.log(`Referrer: ${ref}`);
+
+    const alias = event.queryStringParameters?.alias;
+    if (!alias) throw new Error('Missing alias');
+    const appUsers = process.env.APPUSER.split('|');
+
+    const approved = ref.match(/https?:\/\/(.*)\.twoday\.net\/?/) || appUsers.includes(alias);
+    if (!approved) throw new Error('Inappropriate origin/alias');
+
+    const url = event.queryStringParameters?.url;
+    if (!url) throw new Error('Missing RSS url');
+
+    const feed = await parser.parseURL(url);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(feed)
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: `Error while reading RSS feed: ${err}.` })
+    };
+  }
+};
